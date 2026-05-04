9bd6600ca0150fb29aa39453ad354c18d1d527c9
phanikalyani <39437327+phanikalyani@users.noreply.github.com>
2026-05-04 08:50:07 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:root@localhost:5432/tenderiq"
+DATABASE_URL = "postgresql://postgres:root@localhost:5432/tenderiq_db"
 
 engine = create_engine(DATABASE_URL)
 
