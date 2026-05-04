dab7b5cfa700c19dd7c81805ef20faa3792f4168
phanikalyani <39437327+phanikalyani@users.noreply.github.com>
2026-05-04 08:52:27 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:root@localhost:5432/tenderiq_db"
+# Get DB URL from environment
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Fallback for local development
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///./test.db"
+
+# Fix for Render (VERY IMPORTANT)
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
 
 engine = create_engine(DATABASE_URL)
 
