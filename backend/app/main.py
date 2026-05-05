f280dc0072a876bc07e4b51d9f8267b03c48e936
phanikalyani <39437327+phanikalyani@users.noreply.github.com>
2026-05-05 22:46:31 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,14 @@ app = FastAPI()
 
 # ✅ CREATE TABLES
 Base.metadata.create_all(bind=engine)
-
+# ✅ CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # ✅ IMPORT ROUTES AFTER APP
 from app.routes.auth import router as auth_router
 from app.routes.tender import router as tender_router
@@ -18,14 +25,6 @@ from app.routes.evaluate import router as evaluate_router
 app.include_router(auth_router, prefix="/auth")
 app.include_router(tender_router, prefix="/tenders")
 app.include_router(evaluate_router, prefix="/evaluate")
-# ✅ CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 def root():
